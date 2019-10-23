f85bce150524d0a8638d0b1eb329f193eb65d970
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-10-23 12:44:22 +0200

    gdb script (multi job, multi-image)

--- devPush.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-# https://www.linuxnix.com/how-to-push-docker-images-to-docker-hub-repository/
-
-name="pinkgorillawb/notebook-dev"
-
-docker push $name
-
--- devRebuild.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-name="pinkgorillawb/notebook-dev"
-
-docker build \
-   --no-cache \
-   ./notebook-dev \
-   -t \
-   $name
